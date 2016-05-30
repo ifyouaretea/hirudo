01f9377c0bae97153f5291f942b6a68c6f174c0d
ifyouaretea <furtado1411@gmail.com>
2016-05-30 19:05:37 +0800

    initial

--- hirudo.js
+++ hirudo.js
@@ -6,37 +6,10 @@ var connection = mysql.createConnection({
 	user		: 'sutdadmin',
 	password	: 'sutd1234',
 	database	: 'hirudo',
-	port		: 3306,
-	multipleStatements: true
+	port		: 3306
 });
 
 connection.connect();
-
-router.post('/patient', function(req,res){
-	console.log("RESTFUL API: \t data");
-	isbn13 = req.query.isbn13;
-	user = req.payload._id;
-	responseMessage = {};
-	try{
-		connection.query('select * from book where ISBN13 LIKE ?;', [isbn13] , function(err, rows, fields) {	
-			if (err) throw err;
-			
-			if (rows.length == 0){
-				responseMessage.status = 0;
-			} 
-		});
-	} catch (err){
-		console.log(err);
-		responseMessage.status = 0;
-		res.send(responseMessage);
-	}
-});
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
 
 router.get('/patient', function(req,res){
 	console.log("RESTFUL API: \t data");
